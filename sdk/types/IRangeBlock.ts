ab5deaf40f5d98fc37bfe248d842b4b669b81ee2
ak <56300182+akcgjc007@users.noreply.github.com>
2023-08-01 12:32:44 +0530

    updated types

--- sdk/types/IRangeBlock.ts
+++ sdk/types/IRangeBlock.ts
@@ -1,6 +1,9 @@
 import { IRangeTransaction } from "./IRangeTransaction"
 
 export interface IRangeBlock {
+  hash: string,
   height: number
   transactions: IRangeTransaction[]
+  networks: string,
+  timestamp: string,
 }
--- sdk/types/IRangeTransaction.ts
+++ sdk/types/IRangeTransaction.ts
@@ -1,7 +1,23 @@
 import { IRangeMessage } from './IRangeMessage'
 
+export interface IKeyValuePair {
+  key: string,
+  value: string,
+}
+
+export interface ITransactionEvent {
+  type: string,
+  attributes: IKeyValuePair[],
+}
+
+export interface ITransactionLog {
+  events: ITransactionEvent[],
+}
+
 export interface IRangeTransaction {
   height: number
   messages: IRangeMessage[]
   success: boolean
+  hash: string
+  logs: { events: ITransactionEvent }[]
 }
