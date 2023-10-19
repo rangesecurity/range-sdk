f020e10a74d3604766a7ac8b51ab47d0a5d0774d
Gurbaj Singh <singh.gurbaj5124871@gmail.com>
2023-10-19 13:31:04 +0530

    enhance: block types updated to include types specific by network for neutron-1

--- src/types/chain/IRangeBlock.ts
+++ src/types/chain/IRangeBlock.ts
@@ -4,6 +4,7 @@ import { Osmosis1Trx } from './osmosis-1/IRangeBlockOsmosis1Trx';
 import { Grand1Trx } from './grand-1/IRangeBlockGrand1Trx';
 import { CosmosHub4Trx } from './cosmoshub-4/IRangeBlockCosmosHub4Trx';
 import { Mocha4Trx } from './mocha-4/IRangeBlockMocha4Trx';
+import { Neutron1Trx } from './neutron-1/IRangeBlockMocha4Trx';
 
 interface BlockBase {
   hash: string;
@@ -34,8 +35,14 @@ interface Mocha4Block extends BlockBase {
   network: NetworkEnum.Mocha4;
 }
 
+interface Neutron1Block extends BlockBase {
+  transactions: Neutron1Trx[];
+  network: NetworkEnum.Neutron1;
+}
+
 export type IRangeBlock =
   | Osmosis1Block
   | Grand1Block
   | CosmosHub4Block
-  | Mocha4Block;
+  | Mocha4Block
+  | Neutron1Block;
---
+++ src/types/chain/neutron-1/IRangeBlockNeutron1TrxMsg.ts
@@ -0,0 +1,375 @@
+import { IRangeMessage } from '../IRangeMessage';
+
+enum Neutron1TrxMsgTypes {
+  CosmosBankV1beta1MsgSend = 'cosmos.bank.v1beta1.MsgSend',
+  CosmwasmWasmV1MsgExecuteContract = 'cosmwasm.wasm.v1.MsgExecuteContract',
+  CosmwasmWasmV1MsgInstantiateContract = 'cosmwasm.wasm.v1.MsgInstantiateContract',
+  CosmwasmWasmV1MsgInstantiateContract2 = 'cosmwasm.wasm.v1.MsgInstantiateContract2',
+  CosmwasmWasmV1MsgMigrateContract = 'cosmwasm.wasm.v1.MsgMigrateContract',
+  CosmwasmWasmV1MsgStoreCode = 'cosmwasm.wasm.v1.MsgStoreCode',
+  IbcApplicationsTransferV1MsgTransfer = 'ibc.applications.transfer.v1.MsgTransfer',
+  IbcCoreChannelV1MsgAcknowledgement = 'ibc.core.channel.v1.MsgAcknowledgement',
+  IbcCoreChannelV1MsgRecvPacket = 'ibc.core.channel.v1.MsgRecvPacket',
+  IbcCoreChannelV1MsgTimeout = 'ibc.core.channel.v1.MsgTimeout',
+  IbcCoreClientV1MsgCreateClient = 'ibc.core.client.v1.MsgCreateClient',
+  IbcCoreClientV1MsgUpdateClient = 'ibc.core.client.v1.MsgUpdateClient',
+  OsmosisTokenFactoryV1beta1MsgCreateDenom = 'osmosis.tokenfactory.v1beta1.MsgCreateDenom',
+  OsmosisTokenFactoryV1beta1MsgMint = 'osmosis.tokenfactory.v1beta1.MsgMint',
+}
+
+export type Neutron1TrxMsg =
+  | Neutron1TrxMsgCosmosBankV1beta1MsgSend
+  | Neutron1TrxMsgCosmwasmWasmV1MsgExecuteContract
+  | Neutron1TrxMsgCosmwasmWasmV1MsgInstantiateContract
+  | Neutron1TrxMsgCosmwasmWasmV1MsgInstantiateContract2
+  | Neutron1TrxMsgCosmwasmWasmV1MsgMigrateContract
+  | Neutron1TrxMsgCosmwasmWasmV1MsgStoreCode
+  | Neutron1TrxMsgIbcApplicationsTransferV1MsgTransfer
+  | Neutron1TrxMsgIbcCoreChannelV1MsgAcknowledgement
+  | Neutron1TrxMsgIbcCoreChannelV1MsgRecvPacket
+  | Neutron1TrxMsgIbcCoreChannelV1MsgTimeout
+  | Neutron1TrxMsgIbcCoreClientV1MsgCreateClient
+  | Neutron1TrxMsgIbcCoreClientV1MsgUpdateClient
+  | Neutron1TrxMsgOsmosisTokenFactoryV1beta1MsgCreateDenom
+  | Neutron1TrxMsgOsmosisTokenFactoryV1beta1MsgMintRootObject;
+
+// types for msg type:: /cosmos.bank.v1beta1.MsgSend
+interface Neutron1TrxMsgCosmosBankV1beta1MsgSend extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.CosmosBankV1beta1MsgSend;
+  data: {
+    amount: { denom: string; amount: string }[];
+    toAddress: string;
+    fromAddress: string;
+  };
+}
+
+// types for msg type:: /cosmwasm.wasm.v1.MsgExecuteContract
+interface Neutron1TrxMsgCosmwasmWasmV1MsgExecuteContract extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.CosmwasmWasmV1MsgExecuteContract;
+  data: {
+    msg: string;
+    sender: string;
+    contract: string;
+  };
+}
+
+// types for msg type:: /cosmwasm.wasm.v1.MsgInstantiateContract
+interface Neutron1TrxMsgCosmwasmWasmV1MsgInstantiateContract
+  extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.CosmwasmWasmV1MsgInstantiateContract;
+  data: {
+    msg: string;
+    label: string;
+    codeId: string;
+    sender: string;
+  };
+}
+
+// types for msg type:: /cosmwasm.wasm.v1.MsgInstantiateContract2
+interface Neutron1TrxMsgCosmwasmWasmV1MsgInstantiateContract2
+  extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.CosmwasmWasmV1MsgInstantiateContract2;
+  data: {
+    msg: string;
+    salt: string;
+    label: string;
+    codeId: string;
+    sender: string;
+  };
+}
+
+// types for msg type:: /cosmwasm.wasm.v1.MsgMigrateContract
+interface Neutron1TrxMsgCosmwasmWasmV1MsgMigrateContract extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.CosmwasmWasmV1MsgMigrateContract;
+  data: {
+    msg: string;
+    codeId: string;
+    sender: string;
+    contract: string;
+  };
+}
+
+// types for msg type:: /cosmwasm.wasm.v1.MsgStoreCode
+interface Neutron1TrxMsgCosmwasmWasmV1MsgStoreCode extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.CosmwasmWasmV1MsgStoreCode;
+  data: {
+    sender: string;
+    wasmByteCode: string;
+  };
+}
+
+// types for msg type:: /ibc.applications.transfer.v1.MsgTransfer
+interface Neutron1TrxMsgIbcApplicationsTransferV1MsgTransfer
+  extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.IbcApplicationsTransferV1MsgTransfer;
+  data: {
+    token: {
+      denom: string;
+      amount: string;
+    };
+    sender: string;
+    receiver: string;
+    sourcePort: string;
+    sourceChannel: string;
+    timeoutHeight: {
+      revisionHeight: string;
+      revisionNumber: string;
+    };
+  };
+}
+
+// types for msg type:: /ibc.core.channel.v1.MsgAcknowledgement
+interface Neutron1TrxMsgIbcCoreChannelV1MsgAcknowledgement
+  extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.IbcCoreChannelV1MsgAcknowledgement;
+  data: {
+    packet: {
+      data: string;
+      sequence: string;
+      sourcePort: string;
+      sourceChannel: string;
+      timeoutHeight: Record<string | number | symbol, unknown>; // todo: find example in db
+      destinationPort: string;
+      timeoutTimestamp: string;
+      destinationChannel: string;
+    };
+    signer: string;
+    proofAcked: string;
+    proofHeight: {
+      revisionHeight: string;
+      revisionNumber: string;
+    };
+    acknowledgement: string;
+  };
+}
+
+// types for mgs type:: ibc.core.channel.v1.MsgRecvPacket
+interface Neutron1TrxMsgIbcCoreChannelV1MsgRecvPacket extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.IbcCoreChannelV1MsgRecvPacket;
+  data: {
+    packet: {
+      data: string;
+      sequence: string;
+      sourcePort: string;
+      sourceChannel: string;
+      timeoutHeight: Record<string | number | symbol, unknown>; // todo: find example in db
+      destinationPort: string;
+      timeoutTimestamp: string;
+      destinationChannel: string;
+    };
+    signer: string;
+    proofHeight: {
+      revisionHeight: string;
+      revisionNumber: string;
+    };
+    proofCommitment: string;
+  };
+}
+
+// types for mgs type:: /ibc.core.channel.v1.MsgTimeout
+interface Neutron1TrxMsgIbcCoreChannelV1MsgTimeout extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.IbcCoreChannelV1MsgTimeout;
+  data: {
+    packet: {
+      data: string;
+      sequence: string;
+      sourcePort: string;
+      sourceChannel: string;
+      timeoutHeight: {
+        revisionHeight?: string;
+        revisionNumber?: string;
+      };
+      destinationPort: string;
+      timeoutTimestamp: string;
+      destinationChannel: string;
+    };
+    signer: string;
+    proofHeight: {
+      revisionHeight?: string;
+      revisionNumber?: string;
+    };
+    proofUnreceived: string;
+    nextSequenceRecv: string;
+  };
+}
+
+// types for mgs type:: /ibc.core.client.v1.MsgCreateClient
+interface Neutron1TrxMsgIbcCoreClientV1MsgCreateClient extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.IbcCoreClientV1MsgCreateClient;
+  data: {
+    '@type': string;
+    signer: string;
+    client_state: {
+      '@type': string;
+      chain_id: string;
+      proof_specs: {
+        leaf_spec: {
+          hash: string;
+          length: string;
+          prefix: string;
+          prehash_key: string;
+          prehash_value: string;
+        };
+        max_depth: number;
+        min_depth: number;
+        inner_spec: {
+          hash: string;
+          child_size: number;
+          child_order: number[];
+          empty_child?: unknown;
+          max_prefix_length: number;
+          min_prefix_length: number;
+        };
+        prehash_key_before_comparison: boolean;
+      }[];
+      trust_level: {
+        numerator: string;
+        denominator: string;
+      };
+      upgrade_path: string[];
+      frozen_height: {
+        revision_height: string;
+        revision_number: string;
+      };
+      latest_height: {
+        revision_height: string;
+        revision_number: string;
+      };
+      max_clock_drift: string;
+      trusting_period: string;
+      unbonding_period: string;
+      allow_update_after_expiry: boolean;
+      allow_update_after_misbehaviour: boolean;
+    };
+    consensus_state: {
+      root: {
+        hash: string;
+      };
+      '@type': string;
+      timestamp: string;
+      next_validators_hash: string;
+    };
+  };
+}
+
+// types for mgs type:: ibc.core.client.v1.MsgUpdateClient
+interface Neutron1TrxMsgIbcCoreClientV1MsgUpdateClient extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.IbcCoreClientV1MsgUpdateClient;
+  data: {
+    '@type': string;
+    signer: string;
+    client_id: string;
+    client_message: {
+      '@type': string;
+      signed_header: {
+        commit: {
+          round: number;
+          height: string;
+          block_id: {
+            hash: string;
+            part_set_header: {
+              hash: string;
+              total: number;
+            };
+          };
+          signatures: {
+            signature?: string;
+            timestamp: string;
+            block_id_flag: string;
+            validator_address?: string;
+          }[];
+        };
+        header: {
+          time: string;
+          height: string;
+          version: {
+            app: string;
+            block: string;
+          };
+          app_hash: string;
+          chain_id: string;
+          data_hash: string;
+          evidence_hash: string;
+          last_block_id: {
+            hash: string;
+            part_set_header: {
+              hash: string;
+              total: number;
+            };
+          };
+          consensus_hash: string;
+          validators_hash: string;
+          last_commit_hash: string;
+          proposer_address: string;
+          last_results_hash: string;
+          next_validators_hash: string;
+        };
+      };
+      validator_set: {
+        proposer: {
+          address: string;
+          pub_key: {
+            ed25519: string;
+          };
+          voting_power: string;
+          proposer_priority: string;
+        };
+        validators: {
+          address: string;
+          pub_key: {
+            ed25519: string;
+          };
+          voting_power: string;
+          proposer_priority: string;
+        }[];
+        total_voting_power: string;
+      };
+      trusted_height: {
+        revision_height: string;
+        revision_number: string;
+      };
+      trusted_validators: {
+        proposer: {
+          address: string;
+          pub_key: {
+            ed25519: string;
+          };
+          voting_power: string;
+          proposer_priority: string;
+        };
+        validators: {
+          address: string;
+          pub_key: {
+            ed25519: string;
+          };
+          voting_power: string;
+          proposer_priority: string;
+        }[];
+        total_voting_power: string;
+      };
+    };
+  };
+}
+
+// types for mgs type:: /osmosis.tokenfactory.v1beta1.MsgCreateDenom
+interface Neutron1TrxMsgOsmosisTokenFactoryV1beta1MsgCreateDenom
+  extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.OsmosisTokenFactoryV1beta1MsgCreateDenom;
+  data: {
+    sender: string;
+    subdenom: string;
+  };
+}
+
+// types for mgs type:: /osmosis.tokenfactory.v1beta1.MsgMint
+interface Neutron1TrxMsgOsmosisTokenFactoryV1beta1MsgMintRootObject
+  extends IRangeMessage {
+  type: Neutron1TrxMsgTypes.OsmosisTokenFactoryV1beta1MsgMint;
+  data: {
+    amount: {
+      denom: string;
+      amount: string;
+    };
+    sender: string;
+  };
+}
