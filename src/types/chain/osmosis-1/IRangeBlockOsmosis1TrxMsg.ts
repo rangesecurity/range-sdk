ed1e1eafb0cb703472553f5b07e0ff9d4ecc72c8
Gurbaj Singh <singh.gurbaj5124871@gmail.com>
2023-12-27 11:54:53 +0530

    fix: types for Osmosis1TrxMsgIbcCoreChannelV1MsgChannelOpenTry updated

--- src/types/chain/osmosis-1/IRangeBlockOsmosis1TrxMsg.ts
+++ src/types/chain/osmosis-1/IRangeBlockOsmosis1TrxMsg.ts
@@ -547,32 +547,29 @@ export interface Osmosis1TrxMsgIbcCoreChannelV1MsgChannelOpenTry
 
 // types for mgs type:: /ibc.core.channel.v1.MsgRecvPacket
 export interface Osmosis1TrxMsgIbcCoreChannelV1MsgRecvPacket {
-    type: string;
-    data: Osmosis1TrxMsgIbcCoreChannelV1MsgRecvPacketData;
-}
-interface Osmosis1TrxMsgIbcCoreChannelV1MsgRecvPacketData {
-    packet: Osmosis1TrxMsgIbcCoreChannelV1MsgRecvPacketPacket;
-    proofCommitment: string;
-    proofHeight: Osmosis1TrxMsgIbcCoreChannelV1MsgRecvPacketProofHeight;
+  type: string;
+  data: {
+    packet: {
+      sequence: string;
+      sourcePort: string;
+      sourceChannel: string;
+      destinationPort: string;
+      destinationChannel: string;
+      timeoutTimestamp?: string;
+      data: string;
+      timeoutHeight: {
+        revisionNumber?: string;
+        revisionHeight?: string;
+      };
+    };
+    proofCommitment?: string;
+    proofHeight: {
+      revisionNumber?: string;
+      revisionHeight?: string;
+    };
     signer: string;
+  };
 }
-interface Osmosis1TrxMsgIbcCoreChannelV1MsgRecvPacketPacket {
-    sequence: string;
-    sourcePort: string;
-    sourceChannel: string;
-    destinationPort: string;
-    destinationChannel: string;
-    data: string;
-    timeoutHeight: Osmosis1TrxMsgIbcCoreChannelV1MsgRecvPacketTimeoutHeight;
-}
-interface Osmosis1TrxMsgIbcCoreChannelV1MsgRecvPacketTimeoutHeight {
-    revisionNumber: string;
-    revisionHeight: string;
-}
-interface Osmosis1TrxMsgIbcCoreChannelV1MsgRecvPacketProofHeight {
-    revisionHeight: string;
-}
-
 
 // types for mgs type:: /ibc.core.channel.v1.MsgTimeout
 export interface Osmosis1TrxMsgIbcCoreChannelV1MsgTimeout
