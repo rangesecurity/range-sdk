27be264e4e1040618a206d4e01a7719089b482b1
Gurbaj Singh <singh.gurbaj5124871@gmail.com>
2024-01-02 16:50:38 +0530

    auto-fix: interface type updated for CelestiaTrxMsgIbcCoreClientV1MsgUpdateClient

--- src/types/chain/celestia/IRangeBlockCelestiaTrxMsg.ts
+++ src/types/chain/celestia/IRangeBlockCelestiaTrxMsg.ts
@@ -614,99 +614,97 @@ export interface CelestiaTrxMsgIbcCoreClientV1MsgCreateClient
 }
 
 // types for msg type: /ibc.core.client.v1.MsgUpdateClient
-export interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClient
-  extends IRangeMessage {
-  type: CelestiaTrxMsgTypes.IbcCoreClientV1MsgUpdateClient;
-  data: {
-    signer: string;
-    clientId: string;
-    clientMessage: {
-      '@type': string;
-      signedHeader: {
-        commit: {
-          round: number;
-          height: string;
-          blockId: {
-            hash: string;
-            partSetHeader: {
-              hash: string;
-              total: number;
-            };
-          };
-          signatures: {
-            blockIdFlag: string;
-            signature?: string;
-            timestamp?: string;
-            validatorAddress?: string;
-          }[];
-        };
-        header: {
-          time: string;
-          height: string;
-          appHash: string;
-          chainId: string;
-          version: {
-            app: string;
-            block: string;
-          };
-          dataHash: string;
-          lastBlockId: {
-            hash: string;
-            partSetHeader: {
-              hash: string;
-              total: number;
-            };
-          };
-          evidenceHash: string;
-          consensusHash: string;
-          lastCommitHash: string;
-          validatorsHash: string;
-          lastResultsHash: string;
-          proposerAddress: string;
-          nextValidatorsHash: string;
-        };
-      };
-      validatorSet: {
-        proposer: {
-          pubKey: {
-            ed25519: string;
-          };
-          address: string;
-          votingPower: string;
-        };
-        validators: {
-          pubKey: {
-            ed25519: string;
-          };
-          address: string;
-          votingPower: string;
-        }[];
-        totalVotingPower: string;
-      };
-      trustedHeight: {
-        revisionHeight: string;
-        revisionNumber: string;
-      };
-      trustedValidators: {
-        proposer: {
-          pubKey: {
-            ed25519: string;
-          };
-          address: string;
-          votingPower: string;
-        };
-        validators: {
-          pubKey: {
-            ed25519: string;
-          };
-          address: string;
-          votingPower: string;
-        }[];
-        totalVotingPower: string;
-      };
-    };
-  };
+export interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClient {
+    type: string;
+    data: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientData;
 }
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientData {
+    clientId: string;
+    clientMessage: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientClientMessage;
+    signer: string;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientClientMessage {
+    '@type': string;
+    signedHeader: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientSignedHeader;
+    validatorSet: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientValidatorSet;
+    trustedHeight: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientTrustedHeight;
+    trustedValidators: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientTrustedValidators;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientSignedHeader {
+    header: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientHeader;
+    commit: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientCommit;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientHeader {
+    version: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientVersion;
+    chainId: string;
+    height: string;
+    time: string;
+    lastBlockId: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientLastBlockId;
+    lastCommitHash: string;
+    dataHash: string;
+    validatorsHash: string;
+    nextValidatorsHash: string;
+    consensusHash: string;
+    appHash: string;
+    lastResultsHash: string;
+    evidenceHash: string;
+    proposerAddress: string;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientVersion {
+    block: string;
+    app: string;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientLastBlockId {
+    hash: string;
+    partSetHeader: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientPartSetHeader;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientPartSetHeader {
+    total: number;
+    hash: string;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientCommit {
+    height: string;
+    blockId: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientBlockId;
+    signatures: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientSignaturesItem[];
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientBlockId {
+    hash: string;
+    partSetHeader: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientPartSetHeader;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientSignaturesItem {
+    blockIdFlag: string;
+    validatorAddress?: string;
+    timestamp?: string;
+    signature?: string;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientValidatorSet {
+    validators: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientValidatorsItem[];
+    proposer: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientProposer;
+    totalVotingPower: string;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientValidatorsItem {
+    address: string;
+    pubKey: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientPubKey;
+    votingPower: string;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientPubKey {
+    ed25519: string;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientProposer {
+    address: string;
+    pubKey: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientPubKey;
+    votingPower: string;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientTrustedHeight {
+    revisionNumber: string;
+    revisionHeight: string;
+}
+interface CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientTrustedValidators {
+    validators: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientValidatorsItem[];
+    proposer: CelestiaTrxMsgIbcCoreClientV1MsgUpdateClientProposer;
+    totalVotingPower: string;
+}
+
 
 // types for msg type: /ibc.core.connection.v1.MsgConnectionOpenAck
 export interface CelestiaTrxMsgIbcCoreConnectionV1MsgConnectionOpenAck
