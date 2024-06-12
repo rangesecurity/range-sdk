3b16f7a457407bfb3768f13786cee5b4cbb981ad
Gurbaj Singh <singh.gurbaj5124871@gmail.com>
2024-06-12 18:52:31 +0530

    auto-fix: interface type updated for CelestiaTrxMsgCosmosAuthzV1beta1MsgExec

--- src/types/chain/celestia/IRangeBlockCelestiaTrxMsg.ts
+++ src/types/chain/celestia/IRangeBlockCelestiaTrxMsg.ts
@@ -97,18 +97,25 @@ export interface CelestiaTrxMsgCelestiaBlobV1MsgPayForBlobs
 }
 
 // types for msg type:: /cosmos.authz.v1beta1.MsgExec
-export interface CelestiaTrxMsgCosmosAuthzV1beta1MsgExec extends IRangeMessage {
-  type: CelestiaTrxMsgTypes.CosmosAuthzV1beta1MsgExec;
-  data: {
-    grantee: string;
-    msgs: (
-      | CelestiaTrxMsgCosmosAuthzV1beta1MsgExecDataMsgSend
-      | CelestiaTrxMsgCosmosAuthzV1beta1MsgExecDataMsgGrant
-      | CelestiaTrxMsgCosmosAuthzV1beta1MsgExecDataMsgGrantAllowance
-      | CelestiaTrxMsgCosmosAuthzV1beta1MsgExecDataMsgRevokeAllowance
-    )[];
-  };
+export interface CelestiaTrxMsgCosmosAuthzV1beta1MsgExec {
+    type: string;
+    data: CelestiaTrxMsgCosmosAuthzV1beta1MsgExecData;
 }
+interface CelestiaTrxMsgCosmosAuthzV1beta1MsgExecData {
+    grantee: string;
+    msgs: CelestiaTrxMsgCosmosAuthzV1Beta1MsgExecMsgsItem[];
+}
+interface CelestiaTrxMsgCosmosAuthzV1beta1MsgExecMsgsItem {
+    '@type': string;
+    delegatorAddress: string;
+    validatorAddress: string;
+    amount: CelestiaTrxMsgCosmosAuthzV1beta1MsgExecAmount;
+}
+interface CelestiaTrxMsgCosmosAuthzV1beta1MsgExecAmount {
+    denom: string;
+    amount: string;
+}
+
 
 interface CelestiaTrxMsgCosmosAuthzV1beta1MsgExecDataMsgSend {
   '@type': '/cosmos.bank.v1beta1.MsgSend';
