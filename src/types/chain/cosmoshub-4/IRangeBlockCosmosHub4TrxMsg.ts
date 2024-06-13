83bdda51ea6cf8376a92f5d3a6f74a46ce0bb51f
Gurbaj Singh <singh.gurbaj5124871@gmail.com>
2024-06-13 16:30:31 +0530

    type: fix interface type updated for CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrant

--- src/types/chain/cosmoshub-4/IRangeBlockCosmosHub4TrxMsg.ts
+++ src/types/chain/cosmoshub-4/IRangeBlockCosmosHub4TrxMsg.ts
@@ -85,24 +85,17 @@ export interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExec
 }
 
 // types for msg type:: /cosmos.authz.v1beta1.MsgGrant
-export interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrant {
-    type: string;
-    data: CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrantData;
-}
-interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrantData {
+export interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrant
+  extends IRangeMessage {
+  type: CosmosHub4TrxMsgTypes.CosmosAuthzV1beta1MsgGrant;
+  data: {
     granter: string;
     grantee: string;
-    grant: CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrantGrant;
+    grant:
+      | CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrantDataGrantStakeAuthorization
+      | CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrantDataGrantGenericAuthorization;
+  };
 }
-interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrantGrant {
-    authorization: CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrantAuthorization;
-    expiration: string;
-}
-interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrantAuthorization {
-    '@type': string;
-    msg: string;
-}
-
 
 interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrantDataGrantStakeAuthorization {
   authorization: {
@@ -111,13 +104,20 @@ interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrantDataGrantStakeAuthorization 
       address: string[];
     };
     authorizationType: string;
+    maxTokens?: {
+      denom: string;
+      amount: string;
+    };
   };
   expiration?: string;
 }
 
 interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrantDataGrantGenericAuthorization {
-  '@type': '/cosmos.authz.v1beta1.GenericAuthorization';
-  msg: string;
+  authorization: {
+    '@type': '/cosmos.authz.v1beta1.GenericAuthorization';
+    msg: string;
+  };
+  expiration: string;
 }
 
 // types for msg type:: /cosmos.authz.v1beta1.MsgRevoke
