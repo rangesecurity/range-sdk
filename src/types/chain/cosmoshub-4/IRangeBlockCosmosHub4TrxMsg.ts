aa222139fab7af0f9fb903dd478346f7c7977342
Gurbaj Singh <singh.gurbaj5124871@gmail.com>
2024-06-17 13:14:28 +0530

    type: fix interface type updated for CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExec

--- src/types/chain/cosmoshub-4/IRangeBlockCosmosHub4TrxMsg.ts
+++ src/types/chain/cosmoshub-4/IRangeBlockCosmosHub4TrxMsg.ts
@@ -66,25 +66,64 @@ enum CosmosHub4TrxMsgTypes {
 }
 
 // types for mgs type:: /cosmos.authz.v1beta1.MsgExec
-export interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExec {
-    type: string;
-    data: CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecData;
-}
-interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecData {
+export interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExec
+  extends IRangeMessage {
+  type: CosmosHub4TrxMsgTypes.CosmosAuthzV1beta1MsgExec;
+  data: {
     grantee: string;
-    msgs: CosmosHub4TrxMsgCosmosAuthzV1Beta1MsgExecMsgsItem[];
-}
-interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecMsgsItem {
-    '@type': string;
-    delegatorAddress: string;
-    validatorAddress: string;
-    amount: CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecAmount;
-}
-interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecAmount {
-    denom: string;
-    amount: string;
+    msgs:
+      | CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecDataMsgsTypeMsgSend
+      | CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecDataMsgsTypeMsgDelegate
+      | CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecDataMsgsTypeMsgWithdrawDelegatorReward
+      | CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecDataMsgsTypeMsgSetWithdrawAddress
+      | CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecDataMsgsTypeMsgMsgWithdrawValidatorCommission
+      | CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecDataMsgsTypeMsgVote;
+  };
 }
 
+interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecDataMsgsTypeMsgSend {
+  '@type': '/cosmos.bank.v1beta1.MsgSend';
+  fromAddress: string;
+  toAddress: string;
+  amount: {
+    denom: string;
+    amount: string;
+  }[];
+}
+
+interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecDataMsgsTypeMsgDelegate {
+  '@type': '/cosmos.staking.v1beta1.MsgDelegate';
+  delegatorAddress: string;
+  validatorAddress: string;
+  amount: {
+    denom: string;
+    amount: string;
+  };
+}
+
+interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecDataMsgsTypeMsgWithdrawDelegatorReward {
+  '@type': '/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward';
+  delegatorAddress: string;
+  validatorAddress: string;
+}
+
+interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecDataMsgsTypeMsgSetWithdrawAddress {
+  '@type': '/cosmos.distribution.v1beta1.MsgSetWithdrawAddress';
+  delegatorAddress: string;
+  validatorAddress: string;
+}
+
+interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecDataMsgsTypeMsgMsgWithdrawValidatorCommission {
+  '@type': '/cosmos.distribution.v1beta1.MsgWithdrawValidatorCommission';
+  validatorAddress: string;
+}
+
+interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgExecDataMsgsTypeMsgVote {
+  '@type': '/cosmos.gov.v1beta1.MsgVote';
+  proposalId: string;
+  voter: string;
+  option: string;
+}
 
 // types for msg type:: /cosmos.authz.v1beta1.MsgGrant
 export interface CosmosHub4TrxMsgCosmosAuthzV1beta1MsgGrant
