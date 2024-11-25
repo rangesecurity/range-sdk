cf94b50405de5b647980a88a445d316e0a94010e
Gurbaj Singh <singh.gurbaj5124871@gmail.com>
2024-11-25 14:33:22 +0530

    enhance: qurantine constants exposed to be customed from env variables

--- src/constants.ts
+++ src/constants.ts
@@ -16,6 +16,13 @@ interface Constants {
     POST_DEBUG_ALERT_PATH: string;
   };
 
+  RULE_QUARANTINE: {
+    PER_EXEC_TIME_CUT_OFF_MS: number;
+    AVG_EXEC_TIME_CUT_OFF_MS: number;
+    AVG_EXEC_TIME_DEBUG_ALERT_MS: number;
+    QUARANTINE_TIME_MINS: number;
+  };
+
   BLOCK_CACHE: {
     MAX: number;
   };
@@ -33,6 +40,46 @@ function getEnvVar(key: string, defaultValue?: string): string {
   }
 
   return value;
+}
+
+function getEnvVarNumber(
+  key: string,
+  defaultValue?: number,
+  validations?: {
+    min?: number;
+    max?: number;
+  },
+): number {
+  const value = process.env[key];
+  if ([undefined, null, ''].includes(value) || Number.isNaN(Number(value))) {
+    if ((!defaultValue && defaultValue !== 0) || Number.isNaN(defaultValue)) {
+      throw new Error(`Missing environment variable: ${key}`);
+    }
+
+    return defaultValue;
+  }
+
+  const parsed = Number(value);
+  if (validations) {
+    if (
+      Object.prototype.hasOwnProperty.call(validations, 'min') &&
+      parsed < (validations.min as number)
+    ) {
+      throw new Error(
+        `Environment variable fails validation: min allowed val: ${validations.min}`,
+      );
+    }
+    if (
+      Object.prototype.hasOwnProperty.call(validations, 'max') &&
+      parsed > (validations.max as number)
+    ) {
+      throw new Error(
+        `Environment variable fails validation: max allowed val: ${validations.max}`,
+      );
+    }
+  }
+
+  return parsed;
 }
 
 export const constants: Constants = {
@@ -55,6 +102,41 @@ export const constants: Constants = {
     }) => `/v1.0/rule-group/${args.ruleGroupId}/rule/${args.ruleId}`,
     ACK_ERROR_TASK_PATH: '/v1.0/rule-group/block/error/ack',
     POST_DEBUG_ALERT_PATH: '/v1.0/range-sdk/debug-alert',
+  },
+
+  RULE_QUARANTINE: {
+    PER_EXEC_TIME_CUT_OFF_MS: getEnvVarNumber(
+      'RULE_QUARANTINE_PER_EXEC_TIME_CUT_OFF_MS',
+      15000,
+      {
+        min: 5000,
+        max: 15000,
+      },
+    ),
+    AVG_EXEC_TIME_CUT_OFF_MS: getEnvVarNumber(
+      'RULE_QUARANTINE_AVG_EXEC_TIME_CUT_OFF_MS',
+      1000,
+      {
+        min: 1000,
+        max: 3000,
+      },
+    ),
+    AVG_EXEC_TIME_DEBUG_ALERT_MS: getEnvVarNumber(
+      'RULE_QUARANTINE_AVG_EXEC_TIME_DEBUG_ALERT_MS',
+      400,
+      {
+        min: 400,
+        max: 3000,
+      },
+    ),
+    QUARANTINE_TIME_MINS: getEnvVarNumber(
+      'RULE_QUARANTINE_QUARANTINE_TIME_MINS',
+      15,
+      {
+        min: 5,
+        max: 30,
+      },
+    ),
   },
 
   BLOCK_CACHE: {
