fef835f70a1c3cd4b8070bc61b846dac7d8c3808
ak <56300182+akcgjc007@users.noreply.github.com>
2024-11-29 13:42:56 +0530

    feat: allow custom rpc override using env var

--- src/services/getCosmosClient.ts
+++ src/services/getCosmosClient.ts
@@ -2,46 +2,63 @@ import { CosmosClient } from '../cosmos/CosmosClient';
 import { Network } from '../network';
 import { IRangeNetwork } from '../types/IRangeNetwork';
 
-const COSMOS_PUBLIC_RPC = 'https://cosmoshub.validator.network/';
-const STRIDE_PUBLIC_RPC = 'https://stride-rpc.publicnode.com:443/';
-const OSMOSIS_PUBLIC_RPC = 'https://rpc.osmosis.zone/';
-const OSMOSIS_TEST_RPC = 'https://rpc.osmotest5.osmosis.zone';
-const MOCHA_RPC = 'https://rpc-2.celestia-mocha.com/';
-const CELESTIA_PUBLIC_RPC = 'https://celestia-rpc.mesa.newmetric.xyz/';
-const NOBLE_RPC = 'https://noble-rpc.polkachu.com/';
-const NEUTRON_PUBLIC_RPC = 'https://neutron-rpc.publicnode.com/';
-const NOBLE_TESTNET_RPC = 'https://rpc.testnet.noble.strange.love/';
-const DYMENSION_RPC = 'https://dymension-rpc.publicnode.com:443';
-const DYDX_RPC = 'https://dydx-rpc.publicnode.com:443';
-
-const OSMOSIS_LCD = 'https://lcd.osmosis.zone';
-const COSMOS_LCD = 'https://cosmos-lcd.quickapi.com';
-const DYMENSION_LCD = 'https://dymension-rest.publicnode.com';
-const STRIDE_LCD = 'https://stride-rest.publicnode.com';
-const CELESTIA_LCD = 'https://celestia-rest.publicnode.com';
-const NOBLE_LCD = 'https://noble-api.polkachu.com';
-const NEUTRON_LCD = 'https://lcd-neutron.whispernode.com';
-const DYDX_LCD = 'https://dydx-dao-api.polkachu.com';
+const urlConfig = {
+  'osmosis-1': {
+    rpc: process.env.OSMOSIS_RPC || 'https://rpc.osmosis.zone/',
+  },
+  'cosmoshub-4': {
+    rpc: process.env.COSMOS_RPC || 'https://cosmoshub.validator.network/',
+  },
+  'grand-1': {
+    rpc:
+      process.env.NOBLE_TESTNET_RPC ||
+      'https://rpc.testnet.noble.strange.love/',
+  },
+  'mocha-4': {
+    rpc: process.env.MOCHA_RPC || 'https://rpc-2.celestia-mocha.com/',
+  },
+  'osmo-test-5': {
+    rpc: process.env.OSMOSIS_TEST_RPC || 'https://rpc.osmotest5.osmosis.zone',
+  },
+  celestia: {
+    rpc: process.env.CELESTIA_RPC || 'https://rpc.lavenderfive.com/celestia',
+  },
+  'noble-1': {
+    rpc: process.env.NOBLE_RPC || 'https://noble-rpc.polkachu.com/',
+  },
+  'neutron-1': {
+    rpc: process.env.NEUTRON_RPC || 'https://neutron-rpc.publicnode.com/',
+  },
+  'dydx-mainnet-1': {
+    rpc: process.env.DYDX_RPC || 'https://dydx-rpc.publicnode.com:443',
+  },
+  'dymension_1100-1': {
+    rpc:
+      process.env.DYMENSION_RPC || 'https://dymension-rpc.publicnode.com:443',
+  },
+  'stride-1': {
+    rpc: process.env.STRIDE_RPC || 'https://stride-rpc.publicnode.com:443/',
+  },
+};
 
 const networkToClients: Record<Network, CosmosClient> = {
-  'osmosis-1': new CosmosClient(OSMOSIS_PUBLIC_RPC, OSMOSIS_LCD),
-  'cosmoshub-4': new CosmosClient(COSMOS_PUBLIC_RPC, COSMOS_LCD),
-  celestia: new CosmosClient(CELESTIA_PUBLIC_RPC, CELESTIA_LCD),
-  'stride-1': new CosmosClient(STRIDE_PUBLIC_RPC, STRIDE_LCD),
-  'noble-1': new CosmosClient(NOBLE_RPC, NOBLE_LCD),
-  'neutron-1': new CosmosClient(NEUTRON_PUBLIC_RPC, NEUTRON_LCD),
-  'dydx-mainnet-1': new CosmosClient(DYDX_RPC, DYDX_LCD),
-  'dymension_1100-1': new CosmosClient(DYMENSION_RPC, DYMENSION_LCD),
-
-  'grand-1': new CosmosClient(NOBLE_TESTNET_RPC),
-  'mocha-4': new CosmosClient(MOCHA_RPC),
-  'osmo-test-5': new CosmosClient(OSMOSIS_TEST_RPC),
+  'osmosis-1': new CosmosClient(urlConfig['osmosis-1'].rpc),
+  'cosmoshub-4': new CosmosClient(urlConfig['cosmoshub-4'].rpc),
+  celestia: new CosmosClient(urlConfig.celestia.rpc),
+  'stride-1': new CosmosClient(urlConfig['stride-1'].rpc),
+  'noble-1': new CosmosClient(urlConfig['noble-1'].rpc),
+  'neutron-1': new CosmosClient(urlConfig['neutron-1'].rpc),
+  'dydx-mainnet-1': new CosmosClient(urlConfig['dydx-mainnet-1'].rpc),
+  'dymension_1100-1': new CosmosClient(urlConfig['dymension_1100-1'].rpc),
+  'grand-1': new CosmosClient(urlConfig['grand-1'].rpc),
+  'mocha-4': new CosmosClient(urlConfig['mocha-4'].rpc),
+  'osmo-test-5': new CosmosClient(urlConfig['osmo-test-5'].rpc),
 };
 
 export function getCosmosClient(network: IRangeNetwork): CosmosClient {
   return networkToClients[network];
 }
 
-export function getCustomCosmosClient(rpc: string): CosmosClient {
-  return new CosmosClient(rpc);
+export function getCustomCosmosClient(rpc: string, lcd?: string): CosmosClient {
+  return new CosmosClient(rpc, lcd);
 }
